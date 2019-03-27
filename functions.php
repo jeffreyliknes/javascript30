2ac040dd124bdb1e4a2b9483bfa77f01121867b8
Jeff Liknes <jeff.liknes@gmail.com>
2019-03-26 22:47:40 -0600

    testing

--- functions.php
+++ functions.php
@@ -84,13 +84,11 @@ add_filter( 'stylesheet_uri', 'red_starter_minified_css', 10, 2 );
  */
 function red_starter_scripts() {
 	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
-
-	wp_enqueue_style('ironworks-fontawesome', "https://use.fontawesome.com/releases/v5.5.0/css/all.css" );
-	wp_enqueue_style('ironworks-flickity-css', "https://unpkg.com/flickity@2/dist/flickity.min.css" );
+	
 	wp_enqueue_script( 'red-starter-navigation', get_template_directory_uri() . '/build/js/navigation.min.js', array(), '20151215', true );
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20151215', true );
-	wp_enqueue_script('ironworks-custom-js', get_template_directory_uri() . '/build/js/custom.min.js', array('jquery'), '', true );
-	wp_enqueue_script('ironworks-flickity-js', "https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js" );
+	wp_enqueue_script('ironworks-custom-js', get_template_directory_uri() . '/build/js/custom.min.js', array('jquery'), '20151215', true );
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
